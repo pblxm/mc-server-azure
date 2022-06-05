eab946ad70898e8f9644e7262d7a78daf301a707
pblxm <pablos22335@gmail.com>
2022-06-05 20:22:16 +0200

    Fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,86 +11,82 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "mcserver" {
-    name     = "rg-mcserver001"
+resource "azurerm_resource_group" "mcserver2" {
+    name     = "rg-mcserver002"
     location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "mcserver" {
-    name                  = "vnet-mcserver001"
+resource "azurerm_virtual_network" "mcserver2" {
+    name                  = "vnet-mcserver002"
     address_space         = ["10.0.0.0/16"]
-    resource_group_name   = azurerm_resource_group.mcserver.name
-    location              = azurerm_resource_group.mcserver.location
+    resource_group_name   = azurerm_resource_group.mcserver2.name
+    location              = azurerm_resource_group.mcserver2.location
 }
 
-resource "azurerm_subnet" "public-mcserver" {
-    name                 = "publicsubnet-001"
-    resource_group_name  = azurerm_resource_group.mcserver.name
-    virtual_network_name = azurerm_virtual_network.mcserver.name
+resource "azurerm_subnet" "public-mcserver2" {
+    name                 = "public-subnet-002"
+    resource_group_name  = azurerm_resource_group.mcserver2.name
+    virtual_network_name = azurerm_virtual_network.mcserver2.name
     address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_storage_account" "mcserver" {
-    name                     = "samcserver001"
+resource "azurerm_storage_account" "mcserver2" {
+    name                     = "samcserver002"
     account_tier             = "Standard"
     account_replication_type = "LRS"
-    location                 = azurerm_resource_group.mcserver.location
-    resource_group_name      = azurerm_resource_group.mcserver.name
+    location                 = azurerm_resource_group.mcserver2.location
+    resource_group_name      = azurerm_resource_group.mcserver2.name
 }
 
-resource "azurerm_network_interface" "mcserver" {
-  name                = "nic-mcserver001"
-  location            = azurerm_resource_group.mcserver.location
-  resource_group_name = azurerm_resource_group.mcserver.name
+resource "azurerm_network_interface" "mcserver2" {
+  name                = "nic-mcserver002"
+  location            = azurerm_resource_group.mcserver2.location
+  resource_group_name = azurerm_resource_group.mcserver2.name
 
   ip_configuration {
     name                          = "nic-mcserver001"
-    subnet_id                     = azurerm_subnet.public-mcserver.id
+    subnet_id                     = azurerm_subnet.public-mcserver2.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "10.0.1.10"
   }
 }
 
-resource "azurerm_ssh_public_key" "mcserver" {
+resource "azurerm_ssh_public_key" "mcserver2" {
   name                = "mykey"
-  resource_group_name = azurerm_resource_group.mcserver.name
-  location            = azurerm_resource_group.mcserver.location
+  resource_group_name = azurerm_resource_group.mcserver2.name
+  location            = azurerm_resource_group.mcserver2.location
   public_key          = file("~/.ssh/mykey.pub")
 }
 
-resource "azurerm_virtual_machine" "mcserver" {
-    name                            = "vm-mcserver001"
-    location                        = azurerm_resource_group.mcserver.location
-    resource_group_name             = azurerm_resource_group.mcserver.name
-    network_interface_ids           = [azurerm_network_interface.mcserver.id]
-    vm_size                         = "Standard_DS1_v2"
+resource "azurerm_linux_virtual_machine" "mcserver2" {
+    name                   = "vm-mcserver002"
+    location               = azurerm_resource_group.mcserver2.location
+    resource_group_name    = azurerm_resource_group.mcserver2.name
+    network_interface_ids  = [azurerm_network_interface.mcserver2.id]
+    size                   = "Standard_DS1_v2"
+    admin_username         = "pbl"
+    computer_name          = "mcserver"  
 
-    storage_image_reference {
+    source_image_reference {
       publisher = "Canonical"
-      offer     = "UbuntuServer"
-      sku       = "20.04-LTS"
+      offer     = "0001-com-ubuntu-server-focal"
+      sku       = "20_04-lts-gen2"
       version   = "latest"
     }
 
-    storage_os_disk {
-      name              = "mcserver001-disk"
-      caching           = "ReadWrite"
-      create_option     = "FromImage"
-      managed_disk_type = "Standard_LRS"
+    os_disk {
+      name                 = "mcserver002-disk"
+      caching              = "ReadWrite"
+      storage_account_type = "StandardSSD_LRS"
     }
 
-    os_profile {
-      computer_name  = "mcserver"
-      admin_username = "pbl"
+    admin_ssh_key {
+      username = "pbl"
+      public_key = azurerm_ssh_public_key.mcserver2.public_key
     }
+    disable_password_authentication = true
+}
 
-    os_profile_linux_config {
-      disable_password_authentication = true
-      ssh_keys {
-        path = "/home/pbl/.ssh/mykey.pub"
-        key_data = azurerm_ssh_public_key.mcserver.public_key
-      }
-    }
-
-
+output "server_ip" {
+  value = azurerm_linux_virtual_machine.mcserver2.public_ip_address
 }
