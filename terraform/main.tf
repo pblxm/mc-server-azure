ecf2000f87efb5b953b8ae18c08996a099660739
pblxm <pablos22335@gmail.com>
2022-06-06 00:00:41 +0200

    Add public IP and rename resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,69 +11,67 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "mcserver2" {
-    name     = "rg-mcserver002"
+resource "azurerm_resource_group" "mcserver" {
+    name     = "rg-mcserver01"
     location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "mcserver2" {
-    name                  = "vnet-mcserver002"
+resource "azurerm_virtual_network" "mcserver" {
+    name                  = "vnet-mcserver01"
     address_space         = ["10.0.0.0/16"]
-    resource_group_name   = azurerm_resource_group.mcserver2.name
-    location              = azurerm_resource_group.mcserver2.location
+    resource_group_name   = azurerm_resource_group.mcserver.name
+    location              = azurerm_resource_group.mcserver.location
 }
 
-resource "azurerm_subnet" "public-mcserver2" {
-    name                 = "public-subnet-002"
-    resource_group_name  = azurerm_resource_group.mcserver2.name
-    virtual_network_name = azurerm_virtual_network.mcserver2.name
+resource "azurerm_subnet" "public-mcserver" {
+    name                 = "public-subnet-01"
+    resource_group_name  = azurerm_resource_group.mcserver.name
+    virtual_network_name = azurerm_virtual_network.mcserver.name
     address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_storage_account" "mcserver2" {
-    name                     = "samcserver002"
+resource "azurerm_storage_account" "mcserver" {
+    name                     = "samcserver01"
     account_tier             = "Standard"
     account_replication_type = "LRS"
-    location                 = azurerm_resource_group.mcserver2.location
-    resource_group_name      = azurerm_resource_group.mcserver2.name
+    location                 = azurerm_resource_group.mcserver.location
+    resource_group_name      = azurerm_resource_group.mcserver.name
 }
 
 resource "azurerm_public_ip" "public_ip" {
-  name                = "ip-mcserver002"
-  resource_group_name = azurerm_resource_group.mcserver2.name
-  location            = azurerm_resource_group.mcserver2.location
+  name                = "ip-mcserver01"
+  resource_group_name = azurerm_resource_group.mcserver.name
+  location            = azurerm_resource_group.mcserver.location
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_interface" "mcserver2" {
-  name                = "nic-mcserver002"
-  location            = azurerm_resource_group.mcserver2.location
-  resource_group_name = azurerm_resource_group.mcserver2.name
+resource "azurerm_network_interface" "mcserver" {
+  name                = "nic-mcserver01"
+  location            = azurerm_resource_group.mcserver.location
+  resource_group_name = azurerm_resource_group.mcserver.name
 
   ip_configuration {
-    name                          = "nic-mcserver001"
-    subnet_id                     = azurerm_subnet.public-mcserver2.id
+    name                          = "nic-mcserver01"
+    subnet_id                     = azurerm_subnet.public-mcserver.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "10.0.1.10"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
-resource "azurerm_ssh_public_key" "mcserver2" {
+resource "azurerm_ssh_public_key" "mcserver" {
   name                = "mykey"
-  resource_group_name = azurerm_resource_group.mcserver2.name
-  location            = azurerm_resource_group.mcserver2.location
+  resource_group_name = azurerm_resource_group.mcserver.name
+  location            = azurerm_resource_group.mcserver.location
   public_key          = file("~/.ssh/mykey.pub")
 }
 
-resource "azurerm_linux_virtual_machine" "mcserver2" {
-    name                   = "vm-mcserver002"
-    location               = azurerm_resource_group.mcserver2.location
-    resource_group_name    = azurerm_resource_group.mcserver2.name
-    network_interface_ids  = [azurerm_network_interface.mcserver2.id]
+resource "azurerm_linux_virtual_machine" "mcserver" {
+    name                   = "vm-mcserver01"
+    location               = azurerm_resource_group.mcserver.location
+    resource_group_name    = azurerm_resource_group.mcserver.name
+    network_interface_ids  = [azurerm_network_interface.mcserver.id]
     size                   = "Standard_B2s"
-
-    
     admin_username         = "pbl"
     computer_name          = "mcserver"  
 
@@ -85,18 +83,18 @@ resource "azurerm_linux_virtual_machine" "mcserver2" {
     }
 
     os_disk {
-      name                 = "mcserver002-disk"
+      name                 = "mcserver01-disk"
       caching              = "ReadWrite"
       storage_account_type = "StandardSSD_LRS"
     }
 
     admin_ssh_key {
       username = "pbl"
-      public_key = azurerm_ssh_public_key.mcserver2.public_key
+      public_key = azurerm_ssh_public_key.mcserver.public_key
     }
     disable_password_authentication = true
 }
 
 output "server_ip" {
-  value = azurerm_linux_virtual_machine.mcserver2.public_ip_address
+  value = azurerm_linux_virtual_machine.mcserver.public_ip_address
 }
